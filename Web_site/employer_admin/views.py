033553931698c3552e8db97547ca996fa788dfb8
Poweranimal-web <nikita.guseff@gmail.com>
2022-08-27 22:51:37 +0300

    add exception

--- Web_site/employer_admin/views.py
+++ Web_site/employer_admin/views.py
@@ -11,43 +11,49 @@ from django.core import serializers
 import logging
 logger = logging.getLogger("django")
 def AdminHomePage(request):
-    if (request.session["Check_auth"]==True or request.session["Auth_Check_Admin"]==True):
-        data ={
-            "name":request.session.get("login")
-        }
-        logger.debug(data["name"])
-        return render(request,"admin/e_admin.html",locals())
-    else:
+    try:
+        if (request.session["Check_auth"]==True or request.session["Auth_Check_Admin"]==True):
+            data ={
+                "name":request.session.get("login")
+            }
+            logger.debug(data["name"])
+            return render(request,"admin/e_admin.html",locals())
+        else:
+            return redirect("employer_admin:auth")
+    except KeyError:
         return redirect("employer_admin:auth")
 def AdminProfilePage(request):
-    if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"]==True):
-        admin_data = EmployerAdminAuth.objects.filter(identifier=request.session.get("id"))[0]
-        restaurant_data = Restaurant.objects.filter(identifier=request.session.get("id"))[0]
-        custom_data = Registration.objects.filter(identifier=request.session.get("id"))[0]
-        if request.method == "POST":
-            form = FormRegistation(request.POST, instance=custom_data)
-            form2 = EmployerAdminForm(request.POST, instance=admin_data)
-            form3 = RestaurantForm(request.POST,request.FILES,instance=restaurant_data)
-            if form.is_valid() and form2.is_valid() and form3.is_valid():
-                            form.save()
-                            form2.save()
-                            form3.save()
-                            logger.debug("save")
-            else:
-                logger.debug("didn't save")
-                logger.debug(form.errors)
-                logger.debug(form2.errors)
-                logger.debug(form3.errors)
-        form = FormRegistation(instance=custom_data)
-        form2 = EmployerAdminForm(instance=admin_data)
-        form3 = RestaurantForm(instance=restaurant_data)
-        data = {
-            "form":form,
-            "form2":form2,
-            "form3":form3,
-        }
-        return render(request,"admin/profile_admin.html",data)
-    else:
+    try:
+        if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"]==True):
+            admin_data = EmployerAdminAuth.objects.filter(identifier=request.session.get("id"))[0]
+            restaurant_data = Restaurant.objects.filter(identifier=request.session.get("id"))[0]
+            custom_data = Registration.objects.filter(identifier=request.session.get("id"))[0]
+            if request.method == "POST":
+                form = FormRegistation(request.POST, instance=custom_data)
+                form2 = EmployerAdminForm(request.POST, instance=admin_data)
+                form3 = RestaurantForm(request.POST,request.FILES,instance=restaurant_data)
+                if form.is_valid() and form2.is_valid() and form3.is_valid():
+                                form.save()
+                                form2.save()
+                                form3.save()
+                                logger.debug("save")
+                else:
+                    logger.debug("didn't save")
+                    logger.debug(form.errors)
+                    logger.debug(form2.errors)
+                    logger.debug(form3.errors)
+            form = FormRegistation(instance=custom_data)
+            form2 = EmployerAdminForm(instance=admin_data)
+            form3 = RestaurantForm(instance=restaurant_data)
+            data = {
+                "form":form,
+                "form2":form2,
+                "form3":form3,
+            }
+            return render(request,"admin/profile_admin.html",data)
+        else:
+            return redirect("employer_admin:auth")
+    except KeyError:
         return redirect("employer_admin:auth")
 def AdminAuthPage(request):
     global name, indefifer
@@ -73,60 +79,72 @@ def AdminAuthPage(request):
         form = EAdminAuthForm()
         return render(request, "admin/e_adminform.html", locals())
 def AdminProductPage(request):
-    if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"] == True):
-        restaurant = Restaurant.objects.filter(identifier=request.session.get("id"))[0]
-        name_r = restaurant.id
-        products = Dish.objects.filter(names_of_restaurant=name_r,is_active=True)
-        return render(request,"add_product_page.html",locals())
-    else:
+    try:
+        if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"] == True):
+            restaurant = Restaurant.objects.filter(identifier=request.session.get("id"))[0]
+            name_r = restaurant.id
+            products = Dish.objects.filter(names_of_restaurant=name_r,is_active=True)
+            return render(request,"add_product_page.html",locals())
+        else:
+            return redirect("employer_admin:auth")
+    except KeyError:
         return redirect("employer_admin:auth")
 def AdminProductDetailPage(request):
-    if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"] == True):
-        restaurant = Restaurant.objects.filter(identifier=request.session.get("id"))[0]
-        if request.method == "POST":
-            form = AddProductForm(request.POST,request.FILES)
-            if form.is_valid():
-                num_sybols = 20
-                IN = ''.join(random.choices(string.ascii_uppercase + string.digits, k=num_sybols))
-                form.instance.IN = IN
-                form.instance.names_of_restaurant = restaurant
-                form.save()
-                logger.debug("save")
-                return redirect("employer_admin:product_admin")
-        form = AddProductForm()
-        data ={
-            "form":form,
-            "check":True
-        }
-        return render(request,"add_detail_product.html",data)
-    else:
+    try:
+        if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"] == True):
+            restaurant = Restaurant.objects.filter(identifier=request.session.get("id"))[0]
+            if request.method == "POST":
+                form = AddProductForm(request.POST,request.FILES)
+                if form.is_valid():
+                    num_sybols = 20
+                    IN = ''.join(random.choices(string.ascii_uppercase + string.digits, k=num_sybols))
+                    form.instance.IN = IN
+                    form.instance.names_of_restaurant = restaurant
+                    form.save()
+                    logger.debug("save")
+                    return redirect("employer_admin:product_admin")
+            form = AddProductForm()
+            data ={
+                "form":form,
+                "check":True
+            }
+            return render(request,"add_detail_product.html",data)
+        else:
+            return redirect("employer_admin:auth")
+    except KeyError:
         return redirect("employer_admin:auth")
 def AdminChangeProductDetailPage(request,pk):
-    if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"] == True):
-        product_exist = Dish.objects.filter(id=pk).exists()
-        if product_exist == True:
-            product = Dish.objects.get(id=pk)
-            if request.method == "POST":
-                form = AddProductForm(request.POST,request.FILES,instance=product)
-                if form.is_valid():
-                    form.save()
+    try:
+        if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"] == True):
+            product_exist = Dish.objects.filter(id=pk).exists()
+            if product_exist == True:
+                product = Dish.objects.get(id=pk)
+                if request.method == "POST":
+                    form = AddProductForm(request.POST,request.FILES,instance=product)
+                    if form.is_valid():
+                        form.save()
+                        return redirect("employer_admin:product_admin")
+                elif request.method == "DELETE":
+                    logger.debug(request.method)
+                    Dish.objects.get(id=pk).delete()
                     return redirect("employer_admin:product_admin")
-            elif request.method == "DELETE":
-                logger.debug(request.method)
-                Dish.objects.get(id=pk).delete()
+                form = AddProductForm(instance=product)
+                data = {
+                    "form": form,
+                    "check": False
+                }
+                return render(request, "add_detail_product.html",data)
+            else:
                 return redirect("employer_admin:product_admin")
-            form = AddProductForm(instance=product)
-            data = {
-                "form": form,
-                "check": False
-            }
-            return render(request, "add_detail_product.html",data)
         else:
-            return redirect("employer_admin:product_admin")
-    else:
+            return redirect("employer_admin:auth")
+    except KeyError:
         return redirect("employer_admin:auth")
 def AdminOrderPage(request):
-    if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"] == True):
-        return render(request, "admin/orders_restaraunt.html")
-    else:
+    try:
+        if (request.session["Check_auth"] == True or request.session["Auth_Check_Admin"] == True):
+            return render(request, "admin/orders_restaraunt.html")
+        else:
+            return redirect("employer_admin:auth")
+    except KeyError:
         return redirect("employer_admin:auth")
