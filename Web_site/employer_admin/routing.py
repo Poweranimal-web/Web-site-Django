380baca90f1b8fbb0beacc2398ffd160c480033c
Poweranimal-web <nikita.guseff@gmail.com>
2022-08-31 19:36:01 +0300

    change path to channel web_socket page

--- Web_site/employer_admin/routing.py
+++ Web_site/employer_admin/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
+from django.urls import path
 
 from employer_admin import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/e_admin/product_admin/orders/$', consumers.PracticeConsumer.as_asgi()),
+    path(r'ws/e_admin/product_admin/orders/$', consumers.PracticeConsumer.as_asgi()),
 ]
